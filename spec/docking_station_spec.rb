b85f865f220ba40560bc4d8badc9bacec6643c20
Student <student@Makerss-MacBook-Air.local>
2020-04-16 16:35:35 +0100

    Added to docking station file and spec file

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,5 +3,13 @@ require 'docking_station'
 describe DockingStation do
   it 'release bike from docking station' do
     expect(DockingStation.new).to respond_to(:release_bike)
-  end 
+  end
+
+  it 'Get a bike' do
+    expect((DockingStation.new.release_bike).class).to eq (Bike)
+  end
+
+  it 'Return true to bike.working?' do
+    expect((DockingStation.new.release_bike).working?).to eq (true)
+  end
 end
