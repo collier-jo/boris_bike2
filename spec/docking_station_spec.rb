db5283b477d9834bf06bf47e8d254d49a2079914
Catriona <catrionamairi@blueyonder.co.uk>
2020-04-16 19:16:54 +0100

    Added to docking station file and spec file

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -12,4 +12,13 @@ describe DockingStation do
   it 'Return true to bike.working?' do
     expect((DockingStation.new.release_bike).working?).to eq (true)
   end
+
+  it 'Return bike to docking station' do
+    expect(DockingStation.new).to respond_to(:dock_bike)
+  end
+
+  it "attr_reader" do
+    expect(DockingStation.new).to respond_to(:bikes)
+  end
+  #it { is_expected.to respond_to :bikes }
 end
